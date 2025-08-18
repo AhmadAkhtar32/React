c9df2783ad8b9e96653e1d0aa863b9ef56981bd4
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-18 23:55:38 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/task1.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/task1.js
@@ -5,3 +5,18 @@ If yes, print "Pass".
 If no, print "Fail".
 Also, print whether the number in marks is even or odd using the % operator. */
 
+let marks = 75;
+
+if (marks >=50){
+    console.log("Pass")
+}
+else{
+    console.log("Fail")
+}
+
+if (marks % 2 == 0 ){
+    console.log("Number is even")
+}
+else{
+    console.log("Number is odd")
+}
