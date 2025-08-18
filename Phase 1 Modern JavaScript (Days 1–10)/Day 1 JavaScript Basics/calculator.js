0835d962ad0871af07b77b96eb044b4a5c1d6118
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-19 00:10:29 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/calculator.js
@@ -0,0 +1,20 @@
+let n = 10;
+let m = 5;
+
+let operator = "+";   // you can change this to ("-", "*", "/")
+
+if (operator === "+") {
+    console.log( n + m)
+}
+else if (operator === "-") {
+    console.log( n-m)
+}
+else if (operator === "*") {
+    console.log( n*m)
+}
+else if (operator === "/") {
+    console.log( n/m)
+}
+else{
+    console.log( "Enter a Valid Operator")
+}
