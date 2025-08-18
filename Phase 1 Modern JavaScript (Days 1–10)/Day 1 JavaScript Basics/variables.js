32e416d06bd59ef29dd1054c3937b8e1d660d6ba
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-18 21:58:38 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/variables.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/variables.js
@@ -1,8 +1,16 @@
 // Variables are the Conatainers That store Data
 
+// Always a Good practice to use Let in code as var have chances for bug
+
 let a = "Ahmad";
 {
-    let a =  1;
+    let a = 1;
     console.log(a);
 }
 console.log(a);     //This defines that let is block-scoped 
+
+
+// Constant Can't be changed 
+
+const b = "Ahsan "
+console.log (b)
