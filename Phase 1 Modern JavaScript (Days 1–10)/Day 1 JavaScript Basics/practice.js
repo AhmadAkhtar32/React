d4735e3649d93c5950723c4f9e4ce49568953750
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-18 22:31:54 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 1 JavaScript Basics/practice.js
@@ -0,0 +1,9 @@
+//Q1 : Create a variable of type string and try to add a number to it
+let a = " I am a Computer Scientist !";
+let b = 32;
+
+console.log(a + b)
+
+//Q2 : Find the datatype of Q1
+
+console.log(typeof(a+b))
