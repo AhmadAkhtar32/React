4848b69fe9121d1f5cf99634d0da55a25f689c52
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-19 23:32:57 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 2 Functions & Scope/functions.js
@@ -0,0 +1,13 @@
+// Function is a block of code designed to perform a specific task 
+// It's reusable, so you don't repeat code 
+
+
+//Simple Function
+function add() {
+    let a = 2;
+    let b = 3;
+    let c = a + b;
+    return c;  // return the result
+}
+
+console.log(add()); // 5
