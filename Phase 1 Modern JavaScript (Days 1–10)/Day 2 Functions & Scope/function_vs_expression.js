d114c573f1363c9997251466db4048628906a00f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-19 23:49:31 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 2 Functions & Scope/function_vs_expression.js
@@ -0,0 +1,9 @@
+//  There are two ways of function Creation
+// 1): Function declaration   2): Function Expression
+
+// 1):  Can be called before defination
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
