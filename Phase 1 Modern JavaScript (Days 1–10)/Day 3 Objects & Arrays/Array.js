5e5ee2484ba211c95f32ec66c3b4cca6eeb6b058
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-20 23:15:28 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 3 Objects & Arrays/Array.js
@@ -0,0 +1,10 @@
+// An array is an ordered list of items  
+
+let Fruits = ["Banana", "Apple ", "Mango ", "Pine-Apple", "Cherry"]
+
+let mixed = ["Ahmad", 1 , true , 11.11, null]    // array can store different type of datatypes
+
+console.log(Fruits)
+console.log(Fruits[0])
+console.log(mixed)
+console.log(mixed[2])
