8eb7c20b5cd7d5f5c5271e00b20dbe1d1cf2624e
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-20 22:46:06 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 3 Objects & Arrays/objects.js
@@ -0,0 +1,6 @@
+/* 
+An object is like a real-world thing that has properties (data) and methods (actions).
+Think of a car:
+Properties: color, brand, speed.
+Methods: start(), stop(), honk().
+ */
