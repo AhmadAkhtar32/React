f31bde111c6aa2dfb720433c0259f55b18a23f21
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-20 22:58:12 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 3 Objects & Arrays/objects.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 3 Objects & Arrays/objects.js
@@ -4,3 +4,14 @@ Think of a car:
 Properties: color, brand, speed.
 Methods: start(), stop(), honk().
  */
+
+const bike ={ 
+    company: "United",
+    Model : 2025,
+    Color : "Black",
+    "Engine P" : "150cc",
+    owner: "Ahmad A"
+}
+
+console.log(bike.owner)
+console.log(bike["Engine P"])
