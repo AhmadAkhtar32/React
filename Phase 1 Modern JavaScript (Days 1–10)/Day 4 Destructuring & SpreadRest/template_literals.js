402d13ae6fa7d1c6b6af7c35a87b015215bff070
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-21 23:01:50 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/template_literals.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/template_literals.js
@@ -1,1 +1,11 @@
 //Template literals (also called template strings) are a powerful string feature in JavaScript that use backticks (`) instead of regular quotes. They provide a more flexible way to create and format strings.
+
+// Regular strings
+const regular = "Hello world";
+const alsoRegular = 'Hello world';
+
+// Template literal
+const template = `Hello world`;
+
+
+// The most common use is embedding expressions using ${}
