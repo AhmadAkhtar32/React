98fa08b9ffaba896f0869187ae2389783c1943d4
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-21 22:55:13 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/spread_and_rest_operator.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/spread_and_rest_operator.js
@@ -1,2 +1,19 @@
 // The spread (...) and rest (...) operators in JavaScript use the same syntax but serve different purposes depending on the context.
 
+/* Spread Operator
+The spread operator expands or "spreads out" elements from arrays, objects, or other iterables. */
+
+// Combine Two Arrays
+arr1 = [1,2,3]
+arr2 = [4,5,6]
+
+const combined = [...arr1, ...arr2]
+console.log(combined)     
+
+// Copy An Array
+const copy = [...arr1]
+console.log(copy)
+
+// Pass Array element as function argument
+
+Math.max(...arr1)
