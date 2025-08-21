2b59a03f0814379534d8a5eb5aeceb45f8ad59e1
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-21 23:06:20 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/template_literals.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 4 Destructuring & SpreadRest/template_literals.js
@@ -9,3 +9,15 @@ const template = `Hello world`;
 
 
 // The most common use is embedding expressions using ${}
+const name = "Ahmad";
+const age = 22;
+
+// Old way (concatenation)
+const message1 = "Hello, my name is " + name + " and I'm " + age + " years old.";
+
+// Template literal way
+const message2 = `Hello, my name is ${name} and I'm ${age} years old.`;
+
+// Can include any expression
+const message3 = `Next year I'll be ${age + 1} years old.`;
+const message4 = `My name in caps is ${name.toUpperCase()}.`;
