7a5091ddefd554d4309970e4f8bbe5fabc3f7820
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-22 22:44:12 +0500

    commit

---
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 6 ES6+ Features/for_of_for_in.js
@@ -0,0 +1,3 @@
+/*for...of → iterates values of an iterable (arrays, strings, Maps/Sets, generators).
+
+for...in → iterates keys (property names) of an object (including inherited enumerable props). Order is not guaranteed.*/
