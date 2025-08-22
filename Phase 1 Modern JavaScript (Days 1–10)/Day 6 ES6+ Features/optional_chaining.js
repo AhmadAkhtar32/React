5a645e35ae69c22438de94f1edf64d70630bd16c
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-22 22:45:40 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 6 ES6+ Features/optional_chaining.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 6 ES6+ Features/optional_chaining.js
@@ -1,1 +1,7 @@
 //Safely access deep properties/calls without crashing if something is null or undefined.
+const data = { profile: { name: "Sara" } };
+
+console.log(data.profile?.name);        // "Sara"
+console.log(data.settings?.theme);      // undefined (no error!)
+console.log(data.arr?.[0]);             // element access
+data.sayHi?.();                         // optional function call
