448ab982b383d10d8809bbb81a052e1fd9f641c1
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-22 22:49:21 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 6 ES6+ Features/Nullish_coalescing.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 6 ES6+ Features/Nullish_coalescing.js
@@ -1,1 +1,5 @@
 //Return the right-hand side only if the left-hand side is null or undefined.
+
+const input = 0;
+console.log(input || 10); // 10  (|| treats 0 as falsy)
+console.log(input ?? 10); // 0   (?? keeps 0)
