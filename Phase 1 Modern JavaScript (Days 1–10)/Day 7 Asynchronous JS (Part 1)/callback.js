08e690aa6eafc85a09eb2fe1ea9ecf0aef7b16c7
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-25 23:16:24 +0500

    commit

--- Phase 1 Modern JavaScript (Days 1–10)/Day 7 Asynchronous JS (Part 1)/callback.js
+++ Phase 1 Modern JavaScript (Days 1–10)/Day 7 Asynchronous JS (Part 1)/callback.js
@@ -1,3 +1,4 @@
+// Call Back
 /* Synchronous means the code run in a particular sequence of instructions given in a program
 Each instruction waits for the previous instruction to complete for it's execution */
 
